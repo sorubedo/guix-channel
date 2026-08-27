3494f21de5f422b188c6dfcb43eece87d5c76df5
sorubedo <zhanrenhangg@gmail.com>
2026-08-27 16:15:51 +0800

    Fix librime plugin build with Guix 1.17

--- modules/sorubedo/packages/input-methods.scm
+++ modules/sorubedo/packages/input-methods.scm
@@ -5,22 +5,10 @@
   #:use-module (guix packages)
   #:use-module (guix utils)
   #:use-module (gnu packages ibus)
-  #:use-module (gnu packages lua)
   #:export (librime-with-plugins))
 
-(define librime-lua-source
-  (let ((commit "7f3eca2ce659fc2401b8acb52bd2182b433e12b1"))
-    (origin
-      (method git-fetch)
-      (uri (git-reference
-            (url "https://github.com/hchunhui/librime-lua")
-            (commit commit)))
-      (file-name (git-file-name "librime-lua" commit))
-      (sha256
-       (base32 "06gsjvk5x4rscs78gzrcck1ggl58hnw7hq7hr67i2536qaxq5qlz")))))
-
 (define librime-octagram-source
-  (let ((commit "bd12863f45fbbd5c7db06d5ec8be8987b10253bf"))
+  (let ((commit "bfb168ca33d8b372596fdf2007933f3da1cf360e"))
     (origin
       (method git-fetch)
       (uri (git-reference
@@ -28,7 +16,7 @@
             (commit commit)))
       (file-name (git-file-name "librime-octagram" commit))
       (sha256
-       (base32 "1n6qpfixfdqwabhb6nisqv7zvalj9rnpvqgdf96siqcyszd3pf7g")))))
+       (base32 "0i6s633axxmyr4jl56jc767g5ql417n62lqn7xw6fwmbniyjlfjk")))))
 
 (define-public librime-with-plugins
   (package
@@ -39,18 +27,11 @@
        ((#:phases phases
          #~%standard-phases)
         #~(modify-phases #$phases
-            (add-after 'unpack 'add-plugins
+            (add-after 'install-plugins 'install-octagram-plugin
               (lambda _
                 (copy-recursively #$(this-package-native-input
-                                     "librime-lua-source") "plugins/lua")
-                (copy-recursively #$(this-package-native-input
                                      "librime-octagram-source")
-                                  "plugins/octagram")
-                (substitute* "plugins/lua/CMakeLists.txt"
-                  (((string-append "foreach\\(pkg "
-                                   "lua lua54 lua53 lua52 luajit lua51\\)"))
-                   (string-append "foreach(pkg lua-5.4 "
-                                  "lua lua54 lua53 lua52 luajit lua51)")))))
+                                  "plugins/octagram")))
             (add-after 'install-license-files 'install-plugin-license-files
               (lambda _
                 (install-file "../source/plugins/lua/LICENSE"
@@ -58,14 +39,11 @@
                 (install-file "../source/plugins/octagram/LICENSE"
                               (string-append #$output
                                              "/share/doc/librime-octagram"))))))))
-    (inputs (modify-inputs (package-inputs librime)
-              (append lua-5.4)))
     (native-inputs (append (package-native-inputs librime)
-                           `(("librime-lua-source" ,librime-lua-source)
-                             ("librime-octagram-source" ,librime-octagram-source))))
+                           `(("librime-octagram-source" ,librime-octagram-source))))
     (synopsis "Rime input method engine with Lua and Octagram plugins")
     (description
      "This variant of librime includes the librime-lua scripting plugin and
 the librime-octagram language model plugin.  Both plugins are merged into the
 shared librime library at build time.")
-    (license (list license:bsd-3 license:gpl3))))
+    (license license:bsd-3)))
